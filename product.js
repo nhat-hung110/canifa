0077418383e94576f13b9e448e13a5df04831f7b
Nguyễn Hưng <nguyennhathung110@gmail.com>
2025-07-28 16:22:28 +0700

    [feat] validSignUpForm function

--- product.js
+++ product.js
@@ -405,6 +405,140 @@ const handlePayment = (info) => {
   payBtn.classList.add("active");
 };
 
+const handleSignIn = () => {
+  const signInBtn = $(".user-box-title .sign-in");
+  const signUpBtn = $(".user-box-title .sign-up");
+  const signInBox = $(".sign-in-wrapper");
+  const signUpBox = $(".sign-up-wrapper");
+
+  signInBtn.onclick = () => {
+    signUpBtn.classList.remove("active");
+    signUpBox.classList.remove("active");
+    signInBtn.classList.add("active");
+    signInBox.classList.add("active");
+  };
+
+  signUpBtn.onclick = () => {
+    signInBtn.classList.remove("active");
+    signInBox.classList.remove("active");
+    signUpBtn.classList.add("active");
+    signUpBox.classList.add("active");
+  };
+};
+
+// Hàm valid form:
+// Khi không có value thì show lỗi => done
+// Kiểm tra SĐT => done
+// Kiểm tra độ dài => done
+// Kiểm tra mật khẩu nhập lại => done
+const validSignUpForm = () => {
+  const signUpInputs = $$(".sign-up-wrapper input");
+  const numberInput = $("#number-sign-up-input");
+  const nameInput = $('#fullname-input')
+  const passwordInput = $("#sign-up-password-input");
+  const confirmPasswordInput = $("#confirm-password-input");
+  const signUpBtn = $(".sign-up-btn");
+
+  const showError = (input, message) => {
+    const parentElement = input.parentElement;
+    const errorElement = parentElement.querySelector(".message");
+    errorElement.innerHTML = message;
+  };
+
+  const isEmpty = (input) => {
+    return input.value.trim() == "";
+  };
+
+  const isValidPhone = (value) => {
+    const regex = /^(0|\+84)\d{9}$/;
+    return regex.test(value.trim());
+  };
+
+  const minLength = (value, length) => {
+    return value.length >= length;
+  };
+
+  const isConfirmPassword = (value, valueConfirm) => {
+    return value == valueConfirm;
+  };
+
+  const validForm = (input) => {
+    if (isEmpty(input)) {
+      showError(input, "Bạn phải nhập trường này");
+      input.classList.add("invalid");
+    } else {
+      showError(input, "");
+      input.classList.remove("invalid");
+      if (input == numberInput && !isValidPhone(input.value)) {
+        showError(input, "Phải nhập đúng định dạng số điện thoại");
+        input.classList.add("invalid");
+      }
+      if (input == passwordInput && !minLength(input.value, 6)) {
+        showError(input, "Mật khẩu phải có tối thiểu 6 ký tự");
+        input.classList.add("invalid");
+      }
+      if (
+        input == confirmPasswordInput &&
+        !isConfirmPassword(passwordInput.value, input.value)
+      ) {
+        showError(input, "Mật khẩu nhập lại không đúng");
+        input.classList.add("invalid");
+      }
+    }
+  };
+
+  signUpInputs.forEach((input) => {
+    input.onblur = () => validForm(input);
+    input.oninput = () => {
+      showError(input, "");
+      input.classList.remove("invalid");
+    };
+  });
+
+  // Nếu có input invalid thì hiển thị lỗi
+  // Nếu tất cả đã valid thì hiển thị đăng ký thành công và post dữ liệu lên API
+  signUpBtn.onclick = () => {
+    signUpInputs.forEach((input) => validForm(input));
+
+    const hasInvalidInput = [...signUpInputs].some((input) =>
+      input.classList.contains("invalid")
+    );
+
+    if (!hasInvalidInput) {
+      const userInfo ={
+        number: numberInput.value,
+        user: nameInput.value,
+        password: passwordInput.value
+      }
+      console.log(userInfo)
+      updateUserInfo(userInfo)
+      signUpInputs.forEach((input) => (input.value = ""));
+
+      const toSignUpBtn = $(".to-sign-up-btn");
+      const signInBtn = $(".user-box-title .sign-in");
+      const signUpBtn = $(".user-box-title .sign-up");
+      const signInBox = $(".sign-in-wrapper");
+      const signUpBox = $(".sign-up-wrapper");
+      const successSignUpBox = $(".succes-sign-up");
+
+      signUpBox.classList.remove("active");
+      successSignUpBox.classList.add("active");
+
+      toSignUpBtn.onclick = () => {
+        successSignUpBox.classList.remove("active");
+        signUpBtn.classList.remove("active");
+        signInBtn.classList.add("active");
+        signInBox.classList.add("active");
+      };
+    }
+  };
+};
+
+const updateUserInfo = (data) => {}
+
+validSignUpForm();
+handleSignIn();
+
 fetch(`${productApi}${productType}/${productId}`)
   .then((res) => res.json())
   .then((product) => {
